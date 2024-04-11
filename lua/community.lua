955d6fd5a45de9f0a460b0b8f511db6adc543afc
Aleksandar Vucic <vucinjo@gmail.com>
2024-04-11 20:08:51 +0200

    Add overseer task

--- lua/community.lua
+++ lua/community.lua
@@ -49,6 +49,20 @@ return {
         },
       }
     end,
+    config = function()
+      vim.api.nvim_create_user_command("OverseerRestartLast", function()
+        local overseer = require "overseer"
+        local tasks = overseer.list_tasks { recent_first = true }
+        if vim.tbl_isempty(tasks) then
+          vim.notify("No tasks found", vim.log.levels.WARN)
+        else
+          overseer.run_action(tasks[1], "restart")
+        end
+      end, {})
+    end,
+    opts = {
+      strategy = "toggleterm",
+    },
   },
   {
     "Pocco81/true-zen.nvim",
