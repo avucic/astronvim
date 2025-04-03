6c9864b879bc5c7e3b327d7eb19cd3b07473c664
Aleksandar Vucic <vucinjo@gmail.com>
2025-04-03 19:49:48 +0200

    Clean

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,7 +20,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      local null_ls = require "null-ls"
+      -- local null_ls = require "null-ls"
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
